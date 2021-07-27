743c28ef5cfe2132af6fab6ecd25858d5f597ad4
Anatolii Kotenko <akotenko1313@gmail.com>
2021-07-28 00:23:10 +0300

    bot

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -1,8 +1,28 @@
+// 
 function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
   // Change code below this line
+const totalPrice=orderedQuantity * pricePerDroid + deliveryFee
 
-
+const message=`You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
 
   // Change code above this line
   return message;
+}
+
+// 
+function isAdult(age) {
+  // Change code below this line
+  const passed = age>=18;
+  // Change code above this line
+  return passed;
+}
+
+// 
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  const isMatch = password === 'jqueryismyjam';
+
+  // Change code above this line
+  return isMatch;
 }
