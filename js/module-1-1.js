37d5b3d00b33ec59ea7c614f4bfe8009c78d9e73
Anatolii Kotenko <akotenko1313@gmail.com>
2021-07-28 10:57:35 +0300

    +

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -1,6 +1,7 @@
 // 
 function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
   // Change code below this line
+  
 const totalPrice=orderedQuantity * pricePerDroid + deliveryFee
 
 const message=`You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
@@ -8,6 +9,9 @@ const message=`You ordered droids worth ${totalPrice} credits. Delivery (${deliv
   // Change code above this line
   return message;
 }
+console.log(makeOrderMessage(2, 100, 50));
+console.log(makeOrderMessage(4, 300, 100));
+console.log(makeOrderMessage(10, 70, 200));
 
 // 
 function isAdult(age) {
@@ -16,12 +20,24 @@ function isAdult(age) {
   // Change code above this line
   return passed;
 }
+console.log(isAdult(20));
+console.log(isAdult(14));
+console.log(isAdult(8));
 
 // 
 function isValidPassword(password) {
   const SAVED_PASSWORD = 'jqueryismyjam';
   // Change code below this line
   const isMatch = password === 'jqueryismyjam';
+
+  // Change code above this line
+  return isMatch;
+}
+
+function isValidPassword(password) {
+  const SAVED_PASSWORD = 'jqueryismyjam';
+  // Change code below this line
+  const isMatch = password===SAVED_PASSWORD;
 
   // Change code above this line
   return isMatch;
