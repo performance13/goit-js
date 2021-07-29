6ad542c95986ac2111c0209a155462f8aed2bc87
Anatolii Kotenko <akotenko1313@gmail.com>
2021-07-29 19:17:23 +0300

    +

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -239,4 +239,72 @@ console.log(isNumberNotInRange(20, 50, 24));
 console.log(isNumberNotInRange(20, 50, 76));
 
 // 
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Change code below this line
 
+  if (totalSpent >= 50000) {
+    discount=GOLD_DISCOUNT;
+    
+  }
+  else if (totalSpent >= 20000 && totalSpent < 50000) {
+    discount=SILVER_DISCOUNT;
+   
+  }
+  else if (totalSpent >= 5000 && totalSpent < 20000) {
+    discount=BRONZE_DISCOUNT;
+    
+  }
+  else if (totalSpent < 5000) {
+    discount=BASE_DISCOUNT;
+  }
+  
+  // Change code above this line
+  return discount;
+}
+  
+  getDiscount(137000);
+  getDiscount(46900);
+  getDiscount(8250);
+  getDiscount(1300);
+  getDiscount(5000);
+  getDiscount(20000);
+  getDiscount(50000);
+
+console.log(getDiscount(137000));
+console.log(getDiscount(46900));
+console.log(getDiscount(8250));
+console.log(getDiscount(1300));
+console.log(getDiscount(5000));
+console.log(getDiscount(20000));
+console.log(getDiscount(50000));
+
+// 
+function checkStorage(available, ordered) {
+  let message;
+
+  // Change code below this line
+  
+message = ordered > available ? "Not enough goods in stock!" : "The order is accepted, our manager will contact you";
+
+  // Change code above this line
+  return message;
+}
+
+checkStorage(100, 50);
+checkStorage(100, 130);
+checkStorage(200, 20);
+checkStorage(200, 150);
+checkStorage(150, 180);
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 150));
+console.log(checkStorage(150, 180));
+
+// 
