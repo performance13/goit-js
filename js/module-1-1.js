ca49f64d4e2baa549fea280508cfa4131176a154
Anatolii Kotenko <akotenko1313@gmail.com>
2021-07-29 12:38:28 +0300

    +

--- js/module-1-1.js
+++ js/module-1-1.js
@@ -160,4 +160,32 @@ console.log(checkPassword(`polyhax`));
 console.log(checkPassword(`jqueryismyjam`));
 
 // 
-console.log(9);
+function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+
+  
+	if(ordered === 0){
+      message = `There are no products in the order!`
+    } else if(ordered > available){
+      message = `Your order is too large, there are not enough items in stock!`
+    } else {message = `The order is accepted, our manager will contact you`
+    }
+  // Change code above this line
+  return message;
+}
+checkStorage(100, 50);
+checkStorage(100, 130);
+checkStorage(70, 0);
+checkStorage(200, 20);
+checkStorage(200, 250);
+checkStorage(150, 0);
+
+console.log(checkStorage(100, 50));
+console.log(checkStorage(100, 130));
+console.log(checkStorage(70, 0));
+console.log(checkStorage(200, 20));
+console.log(checkStorage(200, 250));
+console.log(checkStorage(150, 0));
+
+// 
