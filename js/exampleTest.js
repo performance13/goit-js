ed300e39ea405d330669018224f020f6d055d4d1
Anatolii Kotenko <akotenko1313@gmail.com>
2021-08-25 21:18:44 +0300

    +

---
+++ js/exampleTest.js
@@ -0,0 +1,48 @@
+// // Генерация псевдослучайного числа от 0 до 1
+// console.log(Math.random());
+
+// // Округление
+// console.log(Math.round(1.5));
+
+
+// // формула для генерации псевдослучайного числа
+// // Math.random() * (max - min) + min
+
+// // console.log(Math.random() * (50 - 30) + 30);
+
+// // для удобства используем переменные
+// const max = 100;
+// const min = 50;
+// console.log(Math.random() * (max - min) + min);
+
+// // чтобы сделать целое число - округляем
+// const result = Math.round(Math.random() * (max - min) + min);
+
+// console.log(result);
+
+
+
+// const medalsReverse = [
+//     [],
+//     [],
+//     [],
+//     [],
+// ];
+
+// for (let i = 0; )
+  
+
+
+// const add = function (value, ...args) {
+//   console.log(value); // первый аргумент
+//   console.log(args); // массив всех остальных аргументов
+// };
+
+// add(10, 1, 2, 3);
+// add(15, 1, 2, 3, 4, 5);
+
+const planets = ["Earth", "Mars", "Venus"];
+planets[0]; // "Earth"
+planets[2]; // "Venus"
+console.log(planets[0]);
+console.log(planets[2]);
